95e5633e286ad88183873a73305dc0090b56bc10
Scatenarafael <rafascatena@gmail.com>
2023-10-02 22:20:32 -0300

    asgi separate http and websocket routes

--- djchat/djchat/asgi.py
+++ djchat/djchat/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import URLRouter, ProtocolTypeRouter
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djchat.settings")
+
+django_application = get_asgi_application()
+
+from . import urls # noqa isort:skip
+
+application = ProtocolTypeRouter(
+  {
+    "http": get_asgi_application(),
+    # "websocket": URLRouter(urls.websocket_urlpatterns),
+  }
+)
 
 application = get_asgi_application()
